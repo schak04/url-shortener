348183fe0299f2c4261cb9bb75d5292a58a4d065
Saptaparno Chakraborty <schakraborty1086@gmail.com>
2026-01-20 22:53:05 +0530

    make handleShorteningOfURL function async and await URL creation

--- controllers/url.js
+++ controllers/url.js
@@ -6,14 +6,14 @@ function displayUI(_req, res) {
     res.sendFile(path.join(__dirname, '..', 'index.html'));
 }
 
-function handleShorteningOfURL(req, res) {
+async function handleShorteningOfURL(req, res) {
     const {originalURL} = req.body.originalURL;
     if (!originalURL) {
         return res.status(400).send("No URL entered by user! Original URL required!!");
     }
     const shortenedURL = `https://sept.com/${nanoid(6)}`;
     
-    URL.create({
+    await URL.create({
         shortened: shortenedURL,
         redirectTo: originalURL
     })
