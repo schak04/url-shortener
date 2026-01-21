57ae7688175d66cba613e7b66e934f4010fd21ad
Saptaparno Chakraborty <schakraborty1086@gmail.com>
2026-01-21 10:51:17 +0530

    feat: read form data and update it after submission

--- controllers/url.js
+++ controllers/url.js
@@ -1,6 +1,7 @@
 const URL = require('../models/url');
 const {nanoid} = require('nanoid');
 const path = require('path');
+const fs = require('fs/promises');
 
 function displayUI(_req, res) {
     res.sendFile(path.join(__dirname, '..', 'index.html'));
@@ -8,17 +9,22 @@ function displayUI(_req, res) {
 
 async function handleShorteningOfURL(req, res) {
     const originalURL = req.body.originalURL;
+    let messageOnFormSubmission = "";
     if (!originalURL) {
+        messageOnFormSubmission = "No URL received";
         return res.status(400).send("No URL entered by user! Original URL required!!");
     }
     const shortenedURL = `https://sept.com/${nanoid(6)}`;
+    messageOnFormSubmission = `Here is your shortened URL: ${shortenedURL}`;
     
     await URL.create({
         shortened: shortenedURL,
         redirectTo: originalURL
     })
 
-    res.status(201).send("Successfully shortened URL");
+    const data = await fs.readFile(path.join(__dirname, '..', 'index.html'), 'utf8');
+    const finalHTML = data.replace('<p id="shortenedURL"></p>', `<p id="shortenedURL">${messageOnFormSubmission}</p>`)
+    res.send(finalHTML);
 }
 
 module.exports = {
